6a64b0365320800f6e4f81605a214525b5a2ed85
Complikatyed <katye.russell@gmail.com>
2016-05-23 18:55:42 -0500

    Initial version pulling data from internal file

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -1,5 +1,29 @@
-app.controller('ItemListCtrl', function($scope){
+// Angular controllers are iifes
+
+app.controller('ItemListCtrl', function($scope, $http){
 
   $scope.items = [];
+
+  $http.get("./data/items.json")
+    .success(function(itemObject){
+
+      // 'itemObject.items' <- gets just the items from the object
+      var itemCollection = itemObject.items;
+
+      // 'Object.keys(itemCollection)' <-- returns an array of the objects
+      // 'forEach(key)' goes through every key in the array and stores the key
+      Object.keys(itemCollection).forEach(function(key){
+
+        // says give the object at that spot an id property AND
+        // assign with the same value as the key itself...
+        // (bcs that key is actually the object's external id, and we need it inside the array)
+        itemCollection[key].id=key;
+
+        // Push the items into the 'items' array, and give it the id from prev line
+        $scope.items.push(itemCollection[key]);
+      })
+
+
+    });
 
 });
